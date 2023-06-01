ecf11866393fe252f3b03a534870bcc729a70262
otsiorinanahary <otsiorinanahary@groupeastek.mg>
2023-06-01 08:31:36 +0300

    Etat initial

---
+++ front_office/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace front_office.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
